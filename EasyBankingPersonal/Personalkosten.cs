92aae5920b21d4565d23b291ba0a60934270f616
Lucas Vienna <dev@lucasvienna.dev>
2019-05-09 19:11:12 +0200

    moved classes and structs to individual files

---
+++ EasyBankingPersonal/Personalkosten.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EasyBankingPersonal.Datenaustausch
+{
+    /// <summary>
+    /// Object reference for one row of the "Personalkosten" table.
+    /// </summary>
+    public class Personalkosten
+    {
+        /// <summary>
+        /// Column 'Durchschnittsjahresgehalt'
+        /// </summary>
+        public Währung Durchschnittsjahresgehalt { get; }
+
+        /// <summary>
+        /// Column 'KostenProEinstellung'
+        /// </summary>
+        public Währung KostenProEinstellung { get; }
+
+        /// <summary>
+        /// Column 'KostenProEntlassung'
+        /// </summary>
+        public Währung KostenProEntlassung { get; }
+
+        /// <summary>
+        /// Column 'TrainingskostenProTagUndMitarbeiter'
+        /// </summary>
+        public Währung TrainingskostenProTagUndMitarbeiter { get; }
+
+        /// <summary>
+        /// Default constructor. Negative values (inc. 0) are not accepted.
+        /// </summary>
+        /// <param name="durchschnittsjahresgehalt">Column 'Durchschnittsjahresgehalt'</param>
+        /// <param name="kostenProEinstellung">Column 'KostenProEinstellung'</param>
+        /// <param name="kostenProEntlassung">Column 'KostenProEntlassung'</param>
+        /// <param name="trainingskostenProTagUndMitarbeiter">Column 'TrainingskostenProTagUndMitarbeiter'</param>
+        public Personalkosten(
+            Währung durchschnittsjahresgehalt,
+            Währung kostenProEinstellung,
+            Währung kostenProEntlassung,
+            Währung trainingskostenProTagUndMitarbeiter
+        )
+        {
+            if (
+                durchschnittsjahresgehalt < 0 ||
+                kostenProEinstellung < 0 ||
+                kostenProEntlassung < 0 ||
+                trainingskostenProTagUndMitarbeiter < 0
+            )
+            {
+                throw new Exception();
+            }
+
+            Durchschnittsjahresgehalt = durchschnittsjahresgehalt;
+            KostenProEinstellung = kostenProEinstellung;
+            KostenProEntlassung = kostenProEntlassung;
+            TrainingskostenProTagUndMitarbeiter = trainingskostenProTagUndMitarbeiter;
+        }
+    }
+}
---
+++ EasyBankingPersonal/Produkte.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace EasyBankingPersonal.Datenaustausch
+{
+    /// <summary>
+    /// Abstract base class for monetary values relating to 
+    /// the six banking products.
+    /// </summary>
+    public abstract class Produkte
+    {
+        /// <summary>
+        /// Monetary value for Autokredite
+        /// </summary>
+        public Währung Autokredite { get; }
+
+        /// <summary>
+        /// Monetary value for Girokonten
+        /// </summary>
+        public Währung Girokonten { get; }
+
+        /// <summary>
+        /// Monetary value for Hypothekenkredite
+        /// </summary>
+        public Währung Hypothekenkredite { get; }
+
+        /// <summary>
+        /// Monetary value for Konsumkredite
+        /// </summary>
+        public Währung Konsumkredite { get; }
+
+        /// <summary>
+        /// Monetary value for Spareinlagen
+        /// </summary>
+        public Währung Spareinlagen { get; }
+
+        /// <summary>
+        /// Monetary value for Termingelder
+        /// </summary>
+        public Währung Termingelder { get; }
+
+        /// <summary>
+        /// Default constructor. Negative values (exc. 0) are not accepted.
+        /// </summary>
+        /// <param name="konsumkredite">Monetary value for Konsumkredite</param>
+        /// <param name="autokredite">Monetary value for Autokredite</param>
+        /// <param name="hypothekenkredite">Monetary value for Hypothekenkredite</param>
+        /// <param name="girokonten">Monetary value for Girokonten</param>
+        /// <param name="spareinlagen">Monetary value for Spareinlagen</param>
+        /// <param name="termingelder">Monetary value for Termingelder</param>
+        public Produkte(
+            Währung konsumkredite,
+            Währung autokredite,
+            Währung hypothekenkredite,
+            Währung girokonten,
+            Währung spareinlagen,
+            Währung termingelder
+        )
+        {
+            if (
+                konsumkredite < 0 ||
+                autokredite < 0 ||
+                hypothekenkredite < 0 ||
+                girokonten < 0 ||
+                spareinlagen < 0 ||
+                termingelder < 0
+            )
+            {
+                throw new Exception();
+            }
+
+            Autokredite = autokredite;
+            Girokonten = girokonten;
+            Hypothekenkredite = hypothekenkredite;
+            Konsumkredite = konsumkredite;
+            Spareinlagen = spareinlagen;
+            Termingelder = termingelder;
+        }
+    }
+}
